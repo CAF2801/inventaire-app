0a3b3841872b6c3e0351964dc1a7730f2b663018
Charles-Antoine Fournier <charlesantoinef854@gmail.com>
2025-07-10 20:25:34 -0400

    First part of HTML

---
+++ api/liste.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inventaire-app</title>
+    <style>
+        h1 {
+            display: none;
+        }
+    </style>
+</head>
+<body>
+<h1>Inventaire-App</h1>
+<header>
+    <div>
+        <h3><a href="../index.php">Inventaire-App</a></h3>
+    </div>
+    <nav>
+        <ul>
+            <li>
+                <a href="./liste.php">Liste d'anticorps</a>
+            </li>
+            <li>
+                <a href="./ajouter.php">Ajouter un anticorps</a>
+            </li>
+            <li>
+                <a href="./modifier.php">Modifier un anticorps</a>
+            </li>
+            <li>
+                <a href="./supprimer.php">Supprimer un anticorps</a>
+            </li>
+        </ul>
+    </nav>
+</header>
+<main>
+   <table>
+       <thead>
+           <tr>
+               <th>Nom Ac</th>
+               <th>Fluorophore</th>
+               <th>#Catalogue</th>
+               <th>Fournisseur</th>
+               <th>Volume Initiale</th>
+               <th>% Restant</th>
+               <th>Stock</th>
+           </tr>
+       </thead>
+       <tbody>
+        <tr>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
+        </tr>
+       </tbody>
+   </table>
+</main>
+<footer>
+    <div>
+        copyright CAF @ 2025
+    </div>
+    <div>
+        Mon github
+        <div>
+            <img src="#" alt="github">
+        </div>
+    </div>
+</footer>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,3 +1,59 @@
-<?php
-
-echo "Hello World!";
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inventaire-app</title>
+    <style>
+        h1 {
+            display: none;
+        }
+    </style>
+</head>
+<body>
+    <h1>Inventaire-App</h1>
+    <header>
+        <div>
+            <h3><a href="./index.php">Inventaire-App</a></h3>
+        </div>
+        <nav>
+            <ul>
+                <li>
+                    <a href="./api/liste.php">Liste d'anticorps</a>
+                </li>
+                <li>
+                    <a href="./api/ajouter.php">Ajouter un anticorps</a>
+                </li>
+                <li>
+                    <a href="./api/modifier.php">Modifier un anticorps</a>
+                </li>
+                <li>
+                    <a href="./api/supprimer.php">Supprimer un anticorps</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <section>
+            <img src="https://placehold.co/600x400" alt="Image d'un anticorps">
+        </section>
+        <div>
+            <h2>Bienvenue sur Inventaire-App</h2>
+        </div>
+        <div>
+            <button><a href="./api/liste.php">Allez vers votre inventaire -></a></button>
+        </div>
+    </main>
+    <footer>
+        <div>
+            copyright CAF @ 2025
+        </div>
+        <div>
+            Mon github
+            <div>
+                <img src="#" alt="github">
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
